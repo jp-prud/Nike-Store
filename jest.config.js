10e6a153f345c4e566e446d382ad85bfd08bbd75
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-05 23:10:06 -0300

    Chore: Configure the jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "<rootDir>/src/components/**/*.ts",
+    "<rootDir>/src/pages/**/*.ts",
+  ],
+};
