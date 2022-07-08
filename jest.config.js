05b1332276c16fda415c68511484a5dd7d1c8d49
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-08 08:09:06 -0300

    Chore: Initial configs to jest

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
   collectCoverage: false,
   collectCoverageFrom: [
-    "<rootDir>/src/components/**/*.ts",
-    "<rootDir>/src/pages/**/*.ts",
+    '<rootDir>/src/components/**/*.ts',
+    '<rootDir>/src/pages/**/*.ts',
   ],
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
 };
