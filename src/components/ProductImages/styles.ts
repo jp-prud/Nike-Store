f83c6687019e0b7b5fcacb3e2afc65442b7bdd86
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-11 00:29:04 -0300

    Styles: Product Images

---
+++ src/components/ProductImages/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+import Arrow from '../../assets/images/icons/Arrow.svg';
+
+export const Container = styled.section`
+  width: 100%;
+  max-width: 100%;
+
+  img {
+    max-width: 100%;
+    aspect-ratio: 1/1;
+  }
+
+  .swiper-button-prev::after,
+  .swiper-button-next::after {
+    content: url(${Arrow});
+  }
+
+  .swiper-button-prev::after {
+    transform: scaleX(-1);
+  }
+
+  .swiper-pagination-clickable .swiper-pagination-bullet {
+    height: 3px;
+    width: 38px;
+    border-radius: 0;
+    margin: 0 !important;
+  }
+
+  .swiper-pagination-bullet-active {
+    background: ${({ theme }) => theme.colors.gray[900]};
+  }
+
+  @media (max-width: 860px) {
+    max-width: calc(100vw - ${({ theme }) => theme.spacing.xlarge}px);
+  }
+
+  @media (min-width: 860px) {
+    max-width: 558px;
+  }
+`;
