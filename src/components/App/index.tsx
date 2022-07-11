1dc93109c06df0a868d1424a90b78955aab0dbda
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-11 00:32:15 -0300

    Feat: Global Theme Provider

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,16 @@
-import Header from '../Header';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../../assets/styles/global';
+
+import Layout from '../Layout';
+import useTheme from '../../hooks/useTheme';
 
 export default function App() {
-  return <Header />;
+  const { currentTheme } = useTheme();
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <GlobalStyles />
+      <Layout />
+    </ThemeProvider>
+  );
 }
