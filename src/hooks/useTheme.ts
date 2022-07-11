16b48a0798746fc6e45edf5db8a02251bcbabdb2
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-10 22:25:47 -0300

    Feat: Create hook useTheme

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,21 @@
+import { useState, useMemo } from 'react';
+
+import themes from '../assets/styles/themes';
+
+type ObjectKeys = keyof typeof themes;
+
+export default function useTheme() {
+  const [theme, setTheme] = useState('light');
+
+  const currentTheme = useMemo(() => {
+    const activeTheme = theme as ObjectKeys;
+
+    return themes[activeTheme] || themes.light;
+  }, [theme]);
+
+  function handleToggleTheme() {
+    setTheme((prevState) => (prevState === 'light' ? 'dark' : 'light'));
+  }
+
+  return { currentTheme, handleToggleTheme };
+}
