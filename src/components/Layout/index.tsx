8d060ab6aef970d1b146154d8473872dd81e6bf3
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-11 00:31:44 -0300

    Feat: Global Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+import Header from '../Header';
+import Main from '../Main';
+
+import { Container } from './styles';
+
+export default function Layout() {
+  return (
+    <Container>
+      <Header />
+      <Main />
+    </Container>
+  );
+}
