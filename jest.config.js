de90eb860b9222f38345cf47d1e103bff7b751f8
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-11 00:57:30 -0300

    Fix: Import images break jest test

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,9 @@ module.exports = {
     '<rootDir>/src/pages/**/*.ts',
   ],
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/mocks/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/src/mocks/fileMock.ts',
+  },
 };
