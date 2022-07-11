f7b5a8dc3d3e5a3f40121125c342a3171f28f6e5
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-10 22:24:06 -0300

    Style: Create global styles

---
+++ src/assets/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Inter', sans-serif;
+    scroll-behavior: smooth:
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
+  body {
+    background-color: #F1F1F1;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    box-shadow: none;
+    background: none;
+  }
+`;
