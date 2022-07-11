b88f6c47e9606286a392ecd4814e9b7bb649a85f
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-11 00:30:42 -0300

    Styles: Add font-face

--- src/assets/styles/global.ts
+++ src/assets/styles/global.ts
@@ -5,7 +5,7 @@ export default createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    font-family: 'Inter', sans-serif;
+    font-family: 'Montserrat', sans-serif;
     scroll-behavior: smooth:
   }
 
