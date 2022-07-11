f9c0b4bda57073f6b92c292fbfd22d287c46aa86
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-10 22:26:38 -0300

    Chore: Create mocks to links in Side Menu

---
+++ src/components/Header/mocks/links.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: 'Home',
+    link: '/',
+  },
+  {
+    name: 'SNKRS',
+    link: '/',
+  },
+  {
+    name: 'Shoes',
+    link: '/',
+  },
+  {
+    name: 'Jackets',
+    link: '/',
+  },
+  {
+    name: 'Skate',
+    link: '/',
+  },
+];
