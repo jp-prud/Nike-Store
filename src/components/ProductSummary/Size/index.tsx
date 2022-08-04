b8c98c79fda73dc3e428b52834dbe852b22c57a0
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-08-04 19:59:19 -0300

    Feat: Create ProductsSize

---
+++ src/components/ProductSummary/Size/index.tsx
@@ -0,0 +1,26 @@
+import { useState, ReactNode } from 'react';
+
+import { Container } from './styles';
+
+interface ISize {
+  children: ReactNode;
+  isAvailable: boolean;
+}
+
+export default function Size({ children, isAvailable }: ISize) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  function handleSelectedProductSize() {
+    setIsSelected((prevState) => !prevState);
+  }
+
+  return (
+    <Container
+      isAvailable={isAvailable}
+      isSelected={isSelected}
+      onClick={() => handleSelectedProductSize()}
+    >
+      {children}
+    </Container>
+  );
+}
