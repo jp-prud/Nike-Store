ea9bcdbf5395209c7f4bf009c9342e85ad041616
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-08-04 20:04:11 -0300

    Feat: Using react-router-dom in Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from '../../Routes';
 import Header from '../Header';
-import Main from '../Main';
 
 import { Container } from './styles';
 
 export default function Layout() {
   return (
-    <Container>
-      <Header />
-      <Main />
-    </Container>
+    <BrowserRouter>
+      <Container>
+        <Header />
+
+        <Routes />
+      </Container>
+    </BrowserRouter>
   );
 }
