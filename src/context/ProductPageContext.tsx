1d75b5d8fc4f3ac2cbe1ceefbc95b337748accf1
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-08-04 20:01:43 -0300

    Feat: Create ProductContext

---
+++ src/context/ProductPageContext.tsx
@@ -0,0 +1,32 @@
+import { createContext } from 'react';
+
+import {
+  IProductPageContext,
+  IProductPageProvider,
+} from '../types/Product.types';
+
+import productsMock from '../mocks/products';
+
+export const ProductPageContext = createContext<IProductPageContext>({
+  product: undefined,
+});
+
+export default function ProductPageProvider({
+  children,
+  productId = '1',
+}: IProductPageProvider) {
+  const currentProduct = productsMock.find(
+    (product) => product.id === productId,
+  );
+
+  return (
+    <ProductPageContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        product: currentProduct,
+      }}
+    >
+      {children}
+    </ProductPageContext.Provider>
+  );
+}
